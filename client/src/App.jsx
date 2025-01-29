94cf27fb50a66c8d722850333733f03c34bdc4ff
Rabia Zubair <rabiazubair949@gmail.com>
2025-01-29 20:43:41 +0500

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Hero from './Components/Home/Hero.jsx';
+// import Hero from './Components/Home/Hero.jsx';
 import Login from "./Pages/Login.jsx"
-import Working from './Pages/Working.jsx';
-import ContactForm from "./Components/Home/ContactForm.jsx"
+// import Working from './Pages/Working.jsx';
+// import ContactForm from "./Components/Home/ContactForm.jsx"
 import EditPortal from './Pages/EditPortal.jsx';
 import Profile from './Pages/Profile.jsx';
 
@@ -17,11 +17,11 @@ function App() {
       <Routes>
         
     
-        <Route path="/" element={<Hero />} />
-        <Route path="/home" element={<Hero />} />
+        <Route path="/" element={<Login />} />
+        {/* <Route path="/home" element={<Hero />} />
         <Route path="/login" element={<Login />} />
         <Route path="/shop" element={<Working />} />
-        <Route path="/contact" element={<ContactForm />} />
+        <Route path="/contact" element={<ContactForm />} /> */}
         <Route path="/edit/:id" element={<EditPortal />} />
         <Route path="/:id" element={<Profile />} />
       </Routes>
