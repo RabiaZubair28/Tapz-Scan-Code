f6916b961cbbd3ca95a2113ef07003b338aa51f0
Rabia Zubair <rabiazubair949@gmail.com>
2025-02-02 14:48:35 +0500

    commit

--- client/src/Pages/EditPortal03.jsx
+++ client/src/Pages/EditPortal03.jsx
@@ -4388,7 +4388,7 @@ console.log(inputField)
       <div className="flex items-center space-x-6 w-full">
         <div className="flex flex-1 items-center justify-center gap-x-3">
           {(!modal04 && !editClientName) && (
-            <div className="flex flex-col text-md w-[300px] max-w-[300px] font-semibold text-gray-800 pt-1  text-center gap-y-1">
+            <div className="flex flex-col text-md w-[250px] max-w-[250px] font-semibold text-gray-800 pt-1  text-center gap-y-1">
               <span className="text-3xl font-semibold text-white">{clientName}</span>
             </div>
           )}
@@ -4475,7 +4475,7 @@ console.log(inputField)
       <div className="flex items-center space-x-6 w-full">
         <div className="flex flex-1 items-center justify-center gap-x-3">
           {(!modal03 && !editDesignation) && (
-            <div className="flex flex-col text-md w-[300px] max-w-[300px] font-semibold  pt-1  text-center gap-y-1">
+            <div className="flex flex-col text-md w-[250px] max-w-[250px] font-semibold  pt-1  text-center gap-y-1">
               <span className="text-md font-light text-white pt-1 pb-1">{designation}</span>
             </div>
           )}
@@ -4577,7 +4577,7 @@ console.log(inputField)
       <div className="flex items-center space-x-6 w-full">
         <div className="flex flex-1 items-center justify-center gap-x-3">
           {(!modal02 && !editDescription) && (
-            <div className="flex flex-col text-md w-[280px] max-w-[280px] font-light  pt-1 text-center gap-y-2">
+            <div className="flex flex-col text-md w-[250px] max-w-[250px] font-light  pt-1 text-center gap-y-2">
             <span className="text-sm text-white pb-1 max-w-[280px] break-words">{description}</span>
           </div>
           
