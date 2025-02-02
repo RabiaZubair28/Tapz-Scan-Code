56d17512252c7e19be12112bd5da838a7f83fd8c
Rabia Zubair <rabiazubair949@gmail.com>
2025-02-02 16:40:11 +0500

    commit

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -4,35 +4,31 @@ const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Find one user with matching email and password
+    // Find user with matching email & password
     const user = await Client.findOne({ email, password });
 
-    if (user.email != email && user.password != password) {
-      return res.status(401).json({
-        message: "Invalid email & Password",
-      });
-    } else if (user.email != email) {
-      return res.status(401).json({
-        message: "Invalid email",
-      });
-    } else if (user.password != password) {
-      return res.status(401).json({
-        message: "Invalid Password",
-      });
-    } else if (user) {
-      return res.status(200).json({
-        message: "Login successful",
-        userId: user.companyName,
-      });
-    } else {
-      return res.status(401).json({
-        message: "Invalid email or password",
-      });
+    if (!user) {
+      // Check if the email exists separately
+      const emailExists = await Client.findOne({ email });
+      const passwordExists = await Client.findOne({ password });
+      if (!emailExists) {
+        throw new Error("Invalid email");
+      } else if (!passwordExists) {
+        throw new Error("Invalid password");
+      } else {
+        throw new Error("Invalid password & email");
+      }
     }
+
+    return res.status(200).json({
+      message: "Login successful",
+      userId: user.companyName,
+    });
   } catch (error) {
-    console.error("Error during login:", error);
-    return res.status(500).json({
-      message: "Invalid credentials",
+    console.error("Error during login:", error.message);
+
+    return res.status(401).json({
+      message: error.message,
     });
   }
 };
