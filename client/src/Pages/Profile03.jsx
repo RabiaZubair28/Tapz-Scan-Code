936fcd39c8244effc1e3a4678dbe4d49c8eac64b
Rabia Zubair <rabiazubair949@gmail.com>
2025-02-06 19:18:14 +0500

    commit

--- client/src/Pages/Profile03.jsx
+++ client/src/Pages/Profile03.jsx
@@ -1553,7 +1553,7 @@ const [selected, setSelected] = useState("");
   </div>
 )}
  </div>}
-      <div className="px-4">
+ { (location != "") && <div className="px-4">
       <h2 className="text-xl font-semibold text-white mb-3 mt-5">Location</h2>
       <hr className="border-gray-300" />
       <div className="flex flex-col items-center bg-white mx-auto rounded-xl border-4 border-white shadow-md  space-y-4 mt-3">
@@ -1561,7 +1561,7 @@ const [selected, setSelected] = useState("");
          <iframe src={location} width="100%" height="300" allowfullscreen="" loading="lazy" className="rounded-xl"></iframe> 
         )}
       </div>
-      </div>
+      </div>}
       <div className="px-4">
           <h2 className="text-xl font-semibold text-white mb-3 mt-5">Share Profile</h2>
           <hr className="border-gray-300" />
