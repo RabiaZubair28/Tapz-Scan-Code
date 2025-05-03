eca612fb14dabb72c531a617fe63bee67b6b9864
Rabia Zubair <rabiazubair949@gmail.com>
2025-05-04 03:35:59 +0500

    final changes

--- client/src/Pages/Profile11.jsx
+++ client/src/Pages/Profile11.jsx
@@ -425,7 +425,7 @@ END:VCARD`;
               style={{
                 backgroundAttachment: "fixed",
                 backgroundImage:
-                  "url('https://res.cloudinary.com/dxokfhkhu/image/upload/v1746304918/7_t4htza.png')",
+                  "url('https://res.cloudinary.com/dxokfhkhu/image/upload/v1746304910/01_f8ohxq.jpg')",
                 backgroundSize: "contain",
                 backgroundPosition: "center",
               }}
