57a3ba5341820d702e84760889fe3eac5aad5d98
Rabia Zubair <rabiazubair949@gmail.com>
2025-07-17 19:41:23 +0500

    Commit

--- client/src/Pages/Profile04.jsx
+++ client/src/Pages/Profile04.jsx
@@ -323,20 +323,7 @@ END:VCARD`;
     return (
       <section>
         <Helmet>
-          <Helmet>
-            <title>{clientName}</title>
-            <meta name="description" content={description}></meta>
-            <meta property="article:section" content={clientName}></meta>
-            <meta property="og:title" content={clientName}></meta>
-            <meta property="og:description" content={description}></meta>
-            <meta
-              property="og:url"
-              content={`https://www.scan-taps.com/${companyName}`}
-            ></meta>
-            <meta property="og:image" content={logo}></meta>
-            <meta name="twitter:title" content={clientName}></meta>
-            <meta name="twitter:description" content={description}></meta>
-          </Helmet>
+          <title>{clientName}</title>
           <meta name="description" content={description}></meta>
           <meta property="article:section" content={clientName}></meta>
           <meta property="og:title" content={clientName}></meta>
@@ -352,8 +339,7 @@ END:VCARD`;
         <div>
           {show && (
             <div
-              className="qr-modal min-h-screen 
-              bg-gradient-to-tr from-gray-950 via-gray-900 to-gray-800 w-full max-w-md mx-auto shadow-lg flex flex-col items-center justify-center relative"
+              className="qr-modal min-h-screen bg-gradient-to-tr from-gray-950 via-gray-900 to-gray-800 w-full max-w-md mx-auto shadow-lg flex flex-col items-center justify-center relative"
               style={{ backgroundAttachment: "fixed" }}
             >
               <div className="bg-white border-gray-500 rounded-lg pb-8 pt-16 px-10 relative">
@@ -450,20 +436,7 @@ END:VCARD`;
           }>Logout</button> */}
 
               <Helmet>
-                <Helmet>
-                  <title>{clientName}</title>
-                  <meta name="description" content={description}></meta>
-                  <meta property="article:section" content={clientName}></meta>
-                  <meta property="og:title" content={clientName}></meta>
-                  <meta property="og:description" content={description}></meta>
-                  <meta
-                    property="og:url"
-                    content={`https://www.scan-taps.com/${companyName}`}
-                  ></meta>
-                  <meta property="og:image" content={logo}></meta>
-                  <meta name="twitter:title" content={clientName}></meta>
-                  <meta name="twitter:description" content={description}></meta>
-                </Helmet>
+                <title>{clientName}</title>
                 <link
                   rel="icon"
                   href={logo}
