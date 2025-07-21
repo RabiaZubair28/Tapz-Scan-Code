f93f418f00df64c9398f250f3742d0da526892d7
Rabia Zubair <rabiazubair949@gmail.com>
2025-07-21 15:21:15 +0500

    Commit

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1079,7 +1079,7 @@ const Dashboard = () => {
             <div className="col-span-2">
               <button
                 type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded w-full"
+                className="bg-blue-500 text-white px-4 py-3 rounded w-full mb-5"
               >
                 Add Client
               </button>
