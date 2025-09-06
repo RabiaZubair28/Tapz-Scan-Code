b3892345c9acab671ab3a32b6b749fe1be6fe465
Rabia Zubair <rabiazubair949@gmail.com>
2025-09-06 14:58:44 +0500

    Commit

--- client/src/Pages/Profile19.jsx
+++ client/src/Pages/Profile19.jsx
@@ -348,7 +348,7 @@ END:VCARD`;
         <div>
           {show && (
             <div
-              className="qr-modal min-h-screen bg-gradient-to-tr from-gray-950 via-gray-900 to-gray-800 w-full max-w-md mx-auto shadow-lg flex flex-col items-center justify-center relative"
+              className="qr-modal min-h-screen bg-gradient-to-tr from-[#784330] via-[#764a3a] to-[#c7bcaf] w-full max-w-md mx-auto shadow-lg flex flex-col items-center justify-center relative"
               style={{ backgroundAttachment: "fixed" }}
             >
               <div className="bg-white border-gray-500 rounded-lg pb-8 pt-16 px-10 relative">
@@ -455,7 +455,7 @@ END:VCARD`;
               </Helmet>
 
               <div
-                className={`min-h-screen w-full max-w-md mx-auto shadow-lg pb-5 text-center bg-gradient-to-tr from-gray-950 via-gray-900 to-gray-800`}
+                className={`min-h-screen w-full max-w-md mx-auto shadow-lg pb-5 text-center bg-gradient-to-tr from-[#784330] via-[#764a3a] to-[#c7bcaf]`}
                 style={{ backgroundAttachment: "fixed" }}
               >
                 {images && (
@@ -1883,7 +1883,7 @@ END:VCARD`;
   } else {
     return (
       <div
-        className={`min-h-screen w-full max-w-md mx-auto shadow-lg pb-5 text-center flex justify-center align-center bg-gradient-to-tr from-gray-950 via-gray-900 to-gray-800 pt-[25%]`}
+        className={`min-h-screen w-full max-w-md mx-auto shadow-lg pb-5 text-center flex justify-center align-center bg-gradient-to-tr from-[#784330] via-[#764a3a] to-[#c7bcaf] pt-[25%]`}
         style={{ backgroundAttachment: "fixed" }}
       >
         <ScaleLoader
