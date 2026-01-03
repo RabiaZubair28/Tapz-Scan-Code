1ed962bbc5295aaff422da7485e35a09d62c78cd
Rabia Zubair <rabiazubair949@gmail.com>
2026-01-03 15:15:05 +0500

    commit

--- client/src/Pages/Profile25.jsx
+++ client/src/Pages/Profile25.jsx
@@ -465,7 +465,7 @@ END:VCARD`;
                         <img
                           src={logo}
                           alt="profile"
-                          className="w-48 h-48 mx-auto rounded-full border-[5px] border-gray-500 shadow-md"
+                          className="w-48 h-48 mx-auto rounded-full border-[5px] border-gray-50 shadow-md"
                         />
                       </div>
                     </a>
@@ -545,7 +545,7 @@ END:VCARD`;
                     {/* <img src={eye} height={25}></img>
                                     <span style={{display:"flex",alignItems:"center",justifyContent:"center",
                                     }}>&nbsp;{visitCount} &nbsp;&nbsp;&nbsp;&nbsp;</span> */}
-                    <button className="flex items-center justify-center gap-x-2 rounded-lg py-2 px-10  bg-gray-600 hover:bg-gray-500 border-white border-[0.25px]shadow-sm hover:shadow-md ">
+                    <button className="flex items-center justify-center gap-x-2 rounded-lg py-2 px-10  bg-gray-600 hover:bg-gray-500 border-white border-[0.25px]shadow-sm hover:shadow-md text-white">
                       <FaDownload
                         size={20}
                         onClick={downloadContactCard}
