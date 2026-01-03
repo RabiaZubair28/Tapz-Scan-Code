f9fefb690cba4ca3c9fed1636418262186272756
Rabia Zubair <rabiazubair949@gmail.com>
2026-01-03 14:45:31 +0500

    commit

--- client/src/Pages/Profile25.jsx
+++ client/src/Pages/Profile25.jsx
@@ -492,7 +492,7 @@ END:VCARD`;
                   </div>
                 )}
                 <div className="px-6">
-                  <div className="flex flex-col justify-center items-start pt-0.5">
+                  <div className="flex flex-col justify-center items-center pt-0.5">
                     <h2 className="text-lg font-semibold text-white pt-1 ">
                       {name}
                     </h2>
