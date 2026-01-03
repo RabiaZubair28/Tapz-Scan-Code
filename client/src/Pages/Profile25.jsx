e5a0176c1cde900745667abf817ffde71a26a0b0
Rabia Zubair <rabiazubair949@gmail.com>
2026-01-03 20:59:41 +0500

    commit

--- client/src/Pages/Profile25.jsx
+++ client/src/Pages/Profile25.jsx
@@ -545,11 +545,11 @@ END:VCARD`;
                     {/* <img src={eye} height={25}></img>
                                     <span style={{display:"flex",alignItems:"center",justifyContent:"center",
                                     }}>&nbsp;{visitCount} &nbsp;&nbsp;&nbsp;&nbsp;</span> */}
-                    <button className="flex items-center justify-center gap-x-2 rounded-sm py-2 px-10  bg-gray-600 hover:bg-gray-500 border-white border-[0.25px] shadow-sm hover:shadow-md text-white">
+                    <button className="flex items-center justify-center gap-x-2 rounded-md py-2 px-10  bg-white border-white border-[0.25px] shadow-sm hover:shadow-md text-black">
                       <FaDownload
                         size={20}
                         onClick={downloadContactCard}
-                        color="white"
+                        color="black"
                       />
                       <span
                         style={{
