410ab408e8a4d49663a20e4ea8d2581953ef7510
Rabia Zubair <rabiazubair949@gmail.com>
2026-01-19 23:49:29 +0500

    added got to history tabs, all histories panel

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -49,7 +49,7 @@ const Profile = () => {
     const fetchClient = async () => {
       try {
         const response = await axios.get(
-          `https://www.scan-taps.com/api/data/client/${clientId}`
+          `https://www.scan-taps.com/api/data/client/${clientId}`,
         );
         setClient(response.data);
         setLoading(false);
